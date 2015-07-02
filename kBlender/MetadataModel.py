2a8743770d643dac6a872e2c7260eefbe0b04a93
Martin Štěpán <marstep@gmail.com>
2015-07-02 17:09:28 +0200

    Minor synchro

--- kBlender/MetadataModel.py
+++ kBlender/MetadataModel.py
@@ -43,7 +43,7 @@ class MetadataModel:
     def solve(self):
 
         if(sum(self.b) == 0):
-            return CorpusComposition(None, 0, None, 0)
+            return CorpusComposition(None, 0, None, [], 0)
 
         x_min = 0
         x_max = 1
